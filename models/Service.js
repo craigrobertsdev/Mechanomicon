ee560e09a642c4a66a8220080751d22bb9504e1b
LucienPep <xxyyzzoozz.com>
2023-03-23 21:41:14 +1030

    Added parameters to service.js for mechanics details on car

--- models/Service.js
+++ models/Service.js
@@ -12,6 +12,45 @@ Service.init(
             primaryKey: true,
             autoIncrement: true,
         },
+        oilChanged: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        filterChanged: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        brakesChecked: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        coolantChecked: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        batteryChecked: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        suspensionChecked: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        filtersChecked: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        tyresRotated: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        generalInspection: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        additionalNotes: {
+            type: DataTypes.STRING,
+        },
         technician_id: {
             type: DataTypes.INTEGER,
             references: {
