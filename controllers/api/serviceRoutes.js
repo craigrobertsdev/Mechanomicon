57e13636d04cc8f398c27c477cd729d429fe4c12
mushroom <shaelah.mai@gmail.com>
2023-03-28 20:03:14 +1000

    adding small changes

--- controllers/api/serviceRoutes.js
+++ controllers/api/serviceRoutes.js
@@ -1,38 +1,39 @@
 const router = require('express').Router();
-const { Service } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Car, Service } = require('../../models');
 
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newService = await Service.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
 
-    res.status(200).json(newService);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const serviceData = await Service.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!serviceData) {
-      res.status(404).json({ message: 'No service comments to delete!' });
-      return;
+router.post('/', async (req, res) => {
+    try {
+      const serviceData = await Service.create(req.body);
+      res.status(200).json(serviceData);
+    } catch (err) {
+      res.status(400).json(err);
     }
+  });
 
-    res.status(200).json(serviceData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/', async (req, res) => {
+    try {
+
+        //REMOVE LATER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        req.session.user_id = 1
+        //REMOVE LATER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        var serviceData = []
+        const carData = await Car.findAll({ where: { user_id: req.session.user_id } });
+        if (carData.length > 1){
+            for (i = 0; i < carData.length; i++){
+                serviceData.push(carData[i].make + ' ' + carData[i].model);
+                serviceData.push(await Service.findAll({ where: { car_id: carData[i].id } }));
+            }
+        }else{
+            serviceData = await Service.findAll({ where: { car_id: carData[0].id } });
+        }   
+
+        res.status(200).json(serviceData);
+        
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
-module.exports = router;
+  module.exports = router;
