881a0805cea2221a8798b1d94c7966c61afe00d1
Gary Gao <gary@GaryMacBook-Pro.gateway>
2023-04-01 06:35:15 +1030

    added google auth using passport.js

---
+++ config/passport.js
@@ -0,0 +1,55 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const { User } = require("../models");
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing user:", user);
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  console.log("Deserializing user with id:", id);
+  try {
+    const user = await User.findByPk(id);
+    console.log("Deserializing user:", user);
+    done(null, user);
+  } catch (error) {
+    console.log("Error deserializing user", error);
+    done(error, null);
+  }
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3001/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log("Google Strategy called");
+      try {
+        const existingUser = await User.findOne({
+          where: { google_id: profile.id },
+        });
+
+        if (existingUser) {
+          console.log("Existing user found:", existingUser);
+          done(null, existingUser);
+        } else {
+          const newUser = await User.create({
+            google_id: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+          });
+
+          console.log("New user created:", newUser);
+          done(null, newUser);
+        }
+      } catch (error) {
+        console.log("Passport error:", error);
+        done(error, null);
+      }
+    }
+  )
+);
