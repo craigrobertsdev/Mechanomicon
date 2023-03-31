cf5a34297765926775123c2d2230f5585864e8e8
Gary Gao <gary@GaryMacBook-Pro.gateway>
2023-04-01 09:15:08 +1030

    updated google callback url to project domain

--- config/passport.js
+++ config/passport.js
@@ -24,7 +24,9 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/auth/google/callback",
+      callbackURL: "https://mechanomicon.herokuapp.com/auth/google/callback",
+      // For testing locally:
+      // callbackURL: "http://localhost:3001/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       console.log("Google Strategy called");
