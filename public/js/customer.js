9499d5213e5e37590339d0e8e33f619e8f137d16
Gary Gao <gary@GaryMacBook-Pro.gateway>
2023-04-02 02:37:49 +1030

    updated customer dashboard layout

---
+++ public/js/customer.js
@@ -0,0 +1,6 @@
+function openModal() {
+  document.getElementById("editModal").classList.remove("hidden");
+}
+function closeModal() {
+  document.getElementById("editModal").classList.add("hidden");
+}
