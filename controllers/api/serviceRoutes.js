6a2f4dc65ceb7e976a1e73a9607dc6286b1cf507
mushroom <shaelah.mai@gmail.com>
2023-04-03 16:02:00 +1000

    FGHJ

--- controllers/api/serviceRoutes.js
+++ controllers/api/serviceRoutes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
-const { withMechanicAuth } = require("../utils/auth");
-const { User, Car, Service, Job } = require("../models");
+const { withTechnicianAuth } = require("../../utils/auth");
+const { User, Car, Service, Job } = require("../../models");
 
 // when logging in as a manager, gets all data required for the admin dashboard
-router.get("/", withMechanicAuth, async (req, res) => {
+router.get("/", withTechnicianAuth, async (req, res) => {
   // id (for loading the job page later on), service_type, drop_off, length, notes, price, technician (id),
   // car_id (rego, make, model, user_id (name, phone number))
+  try {
   const jobData = Job.findAll({
     attributes: ["id", "type", "date", "notes", "drop_off", "completed"],
     include: [
@@ -16,14 +17,14 @@ router.get("/", withMechanicAuth, async (req, res) => {
     ],
   });
 
-const [ jobs, services] = await Promise.all([
+  const [ jobs, services] = await Promise.all([
   jobData,
   serviceData,
-]);
+  ]);
 
-const serialisedJobData = jobs.map((job) => job.get({ plain: true }));
-  const serialisedServiceData = services.map((service) =>
-    service.get({ plain: true })
+  const serialisedJobData = jobs.map((job) => job.get({ plain: true }));
+    const serialisedServiceData = services.map((service) =>
+      service.get({ plain: true })
   );
 
   res.render("workshopDashboard", {
@@ -34,37 +35,40 @@ const serialisedJobData = jobs.map((job) => job.get({ plain: true }));
     servicesJSON: JSON.stringify(services),
     logged_in: req.session.logged_in,
   });
-} );
-
-router.get('/job/:id', async (req, res) => {
-  const jobData = Job.findAll({
-    attributes: ["id", "type", "date", "notes", "drop_off", "completed"],
-    include: [
-      {
-        model: Car,
-        attributes: ["id", "license_plate", "make", "model", "colour", "year"],
-      },
-    ],
-  });
-});
-
-const jobs = await Promise.all(jobData);
-
-const serialisedJobData = jobs.map((job) => job.get({ plain: true }));
-
-res.render("workshopDashboard", {
-  jobs: serialisedJobData,
-  jobsJSON: JSON.stringify(jobs),
-});
-
-router.post("/", async (req, res) => {
-  try {
-    const serviceData = await Service.create(req.body);
-    res.status(200).json(serviceData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
+
+// router.get('/job/:id', async (req, res) => {
+//   const jobData = Job.findAll({
+//     attributes: ["id", "type", "date", "notes", "drop_off", "completed"],
+//     include: [
+//       {
+//         model: Car,
+//         attributes: ["id", "license_plate", "make", "model", "colour", "year"],
+//       },
+//     ],
+//   });
+// });
+
+// const jobs = await Promise.all(jobData);
+
+// const serialisedJobData = jobs.map((job) => job.get({ plain: true }));
+
+// res.render("workshopDashboard", {
+//   jobs: serialisedJobData,
+//   jobsJSON: JSON.stringify(jobs),
+// });
+
+// router.post("/", async (req, res) => {
+//   try {
+//     const serviceData = await Service.create(req.body);
+//     res.status(200).json(serviceData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 router.get("/", async (req, res) => {
   try {
